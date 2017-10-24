974c7f7e5a0e4b275b74845fc0b3c171ef588b49
Manuj Bhatia <m@manujbhatia.net>
2017-10-24 14:12:41 +0000

    Change all Twilio settings to env vars

--- winner.php
+++ winner.php
@@ -4,13 +4,13 @@
 
   $client = new Services_Twilio($_ENV['TWILIO_ACCOUNT_SID'], $_ENV['TWILIO_AUTH_TOKEN']);
 
-  $messages = $client->account->messages->getPage(0, 100, array('To' => '+17274740846'))->getItems();
+  $messages = $client->account->messages->getPage(0, 100, array('To' => $_ENV['TWILIO_PHONE_NUMBER']))->getItems();
 
   $numbers = array_unique(array_map(function($x){return $x->from;}, $messages));
 
   $winner = $numbers[array_rand($numbers)];
 
-  $client->account->calls->create('+17274740846', $winner, 'https://650fb145.ngrok.com/congrats.php');
+  $client->account->calls->create($_ENV['TWILIO_PHONE_NUMBER'], $winner, $_ENV['TWILIO_BASE_URL'] . '/congrats.php');
 
   echo "We have a winner: " . substr($winner, -4);
 
